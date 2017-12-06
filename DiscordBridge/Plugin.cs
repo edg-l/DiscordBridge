11a3dabbe43b3fdaadd6c9187389d0d35679ca7e
Edgar Luque <clud6565@gmail.com>
2017-12-06 16:10:39 +0100

    Add project files.

---
+++ DiscordBridge/Plugin.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.Data;
+using System.IO;
+using Mono.Data.Sqlite;
+using System.Reflection;
+using TShockAPI.DB;
+using MySql.Data.MySqlClient;
+
+namespace DiscordBridge
+{
+    [ApiVersion(2, 1)]
+    public class DiscordBridge : TerrariaPlugin
+    {
+        #region Plugin Info
+        public override string Name => "DiscordBridge";
+        public override string Author => "Ryozuki";
+        public override string Description => "I do this and that.";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        #endregion
+
+        public Util.ConfigFile Config = new Util.ConfigFile();
+
+        public DiscordBridge(Main game) : base(game)
+        {
+        }
+
+        private void LoadConfig()
+        {
+            string path = Path.Combine(TShock.SavePath, "DiscordBridge.json");
+            Config = Util.ConfigFile.Read(path);
+        }
+
+        public override void Initialize()
+        {
+            LoadConfig();
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+            ServerApi.Hooks.GamePostInitialize.Register(this, OnPostInitialize);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+                ServerApi.Hooks.GamePostInitialize.Deregister(this, OnPostInitialize);
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Hooks
+        private void OnInitialize(EventArgs args)
+        {
+            Util.DBHelper.Connect();
+            /*Util.DBHelper.CreateTable(new SqlTable("DiscordBridge",
+                new SqlColumn("ID", MySqlDbType.Int32) { Primary = true, Unique = true, Length = 7, AutoIncrement = true },
+                new SqlColumn("UserID", MySqlDbType.Int32) { Length = 6 },
+                new SqlColumn("Balance", MySqlDbType.Float) { DefaultValue = "0" }
+                ));*/
+
+            Commands.ChatCommands.Add(new Command("DiscordBridge.help".ToLower(), CHelp, "chelp")
+            {
+                HelpText = "Usage: /chelp"
+            });
+        }
+
+        private void OnPostInitialize(EventArgs args)
+        {
+
+        }
+        #endregion
+
+        #region Commands
+        private void CHelp(CommandArgs args)
+        {
+            args.Player.SendInfoMessage("Author, please change me.");
+        }
+        #endregion
+    }
+}
---
+++ DiscordBridge/Util/DBHelper.cs
@@ -0,0 +1,109 @@
+﻿using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace DiscordBridge.Util
+{
+    public static class DBHelper
+    {
+        public static IDbConnection db;
+        public static SqlTableCreator sqlcreator;
+
+        public static async Task<bool> CreateTableAsync(SqlTable table)
+        {
+            return await Task.Run(() => sqlcreator.EnsureTableStructure(table));
+        }
+
+        public static bool CreateTable(SqlTable table)
+        {
+            return sqlcreator.EnsureTableStructure(table);
+        }
+
+        public static async Task ConnectAsync()
+        {
+            await Task.Run(() =>
+            {
+                switch (TShock.Config.StorageType.ToLower())
+                {
+                    case "mysql":
+                        string[] dbHost = TShock.Config.MySqlHost.Split(':');
+                        db = new MySqlConnection()
+                        {
+                            ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                                dbHost[0],
+                                dbHost.Length == 1 ? "3306" : dbHost[1],
+                                TShock.Config.MySqlDbName,
+                                TShock.Config.MySqlUsername,
+                                TShock.Config.MySqlPassword)
+
+                        };
+                        break;
+
+                    case "sqlite":
+                        string sql = Path.Combine(TShock.SavePath, "DiscordBridge.sqlite");
+                        db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+                        break;
+                }
+
+                sqlcreator = new SqlTableCreator(db,
+                    db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+            });
+        }
+
+        public static void Connect()
+        {
+            switch (TShock.Config.StorageType.ToLower())
+            {
+                case "mysql":
+                    string[] dbHost = TShock.Config.MySqlHost.Split(':');
+                    db = new MySqlConnection()
+                    {
+                        ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                            dbHost[0],
+                            dbHost.Length == 1 ? "3306" : dbHost[1],
+                            TShock.Config.MySqlDbName,
+                            TShock.Config.MySqlUsername,
+                            TShock.Config.MySqlPassword)
+
+                    };
+                    break;
+
+                case "sqlite":
+                    string sql = Path.Combine(TShock.SavePath, "DiscordBridge.sqlite");
+                    db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+                    break;
+            }
+
+            sqlcreator = new SqlTableCreator(db,
+                db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+        }
+
+        public static async Task<int> QueryAsync(string query, params object[] args)
+        {
+            return await Task.Run(() => db.Query(query, args));
+        }
+
+        public static int Query(string query, params object[] args)
+        {
+            return db.Query(query, args);
+        }
+
+        public static async Task<QueryResult> QueryResultAsync(string query, params object[] args)
+        {
+            return await Task.Run(() => db.QueryReader(query, args));
+        }
+
+        public static QueryResult QueryResult(string query, params object[] args)
+        {
+            return db.QueryReader(query, args);
+        }
+    }
+}
